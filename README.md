ce007490d85027e7b9790ac0cce4842792c4d10c
tyno43 <tnyo43@gmail.com>
2020-08-20 00:21:44 +0900

    add: options

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ kotlinc src/*.kt
 
 ## run
 
-kotlin MainKt
+kotlin MainKt <option>
+
+### options
+
+- -h : アクセスの多いリモートホストの順にアクセス件数の一覧を表示する
+- -t : 各時間帯毎のアクセス件数を表示する
+- -s : 総アクセス件数を表示する
--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,23 @@
-fun main() {
+fun getDisplayClass(option: String, log: AccessLog): Display =
+    when (option) {
+        "-h" -> HostDisplay(log)
+        "-t" -> TimezoneDisplay(log)
+        "-s" -> SimpleDisplay(log)
+        else -> throw Exception("Unknown option Exception");
+    }
+
+
+fun main(args: Array<String>) {
+    if (args.size == 0) {
+        println("オプションを指定してください");
+        println("Example 1: kotlin MainKt -s -> 総アクセス件数を表示");
+        println("Example 2: kotlin MainKt -t -> 時間帯ごとのアクセス件数を表示");
+        println("Example 3: kotlin MainKt -h -> ホストごとのアクセス件数を表示");
+    }
+
     val log = AccessLog();
-    log.readLogFile("log.txt");
+    log.readLogFile("log_sample.txt");
 
-    var display: Display = SimpleDisplay(log);
-    display.display();
-    println();
-
-    display = TimezoneDisplay(log);
-    display.display();
-    println();
-
-    display = HostDisplay(log);
+    val display: Display = getDisplayClass(args[0], log)
     display.display();
 }
